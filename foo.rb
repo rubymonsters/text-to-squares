6af19e1480ec3e7ab16c17e121dcf107c2f94986
Sven Fuchs <svenfuchs@artweb-design.de>
2012-07-30 21:08:15 +0200

    use an options hash

--- foo.rb
+++ foo.rb
@@ -3,26 +3,34 @@ class User
     @name = name
   end
 
+  def name
+    @name
+  end
+
   def introduce
     puts 'Hi! My name is ' + @name
   end
 
-  def send_email(recipient, subject, body)
-    email = Email.new(recipient, subject, body, @name)
+  def send_email(recipient, subject, body = 'Default text', options = {})
+    options[:sender] = @name
+    email = Email.new(recipient, subject, body, options)
     email.send
   end
 end
 
 class Email
-  def initialize(recipient, subject, body, sender)
+  def initialize(recipient, subject, body, options = {})
+    # options[:sender] = options[:sender] || 'unknown sender'
+    options[:sender] ||= 'unknown sender'
+
     @recipient = recipient
     @subject = subject
     @body = body
-    @sender = sender
+    @options = options
   end
 
   def send
-    puts %(sending an email from #{@sender} to #{@recipient} with the subject "#{@subject}" and the body "#{@body}")
+    puts %(sending an email from #{@options[:sender]} to #{@recipient} with the subject "#{@subject}" and the body "#{@body}")
   end
 end
 
@@ -32,4 +40,16 @@ sabrina = User.new('Sabrina')
 anja.introduce
 sabrina.introduce
 
-anja.send_email('carla@email.com', 'Railsgirls infos!', 'Lots of text here ...')
+puts anja.name
+
+anja.send_email('carla@email.com', 'Railsgirls infos!')
+
+
+def juggle_with(*things)
+  p things
+end
+
+juggle_with(1)
+juggle_with(1, :foo)
+juggle_with(1, anja.name, User.new('Bettina'), Email.new('carla@email.com', 'Yay, juggling an email!', 'Some text'))
+juggle_with(1, 2, 3, 4)
