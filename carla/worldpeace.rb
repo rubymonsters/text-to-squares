e1d7ee82c3156d0a866abdf6a82e23b6f9cd74a5
Sven Fuchs <svenfuchs@artweb-design.de>
2012-08-06 20:19:51 +0200

    mess around with carlas pretty code, im so sooooorry

--- carla/worldpeace.rb
+++ carla/worldpeace.rb
@@ -1,19 +1,31 @@
 # I'm trying to create world peace.
 # My premise is that for wold peace to exist all countries must
 # have optimal levels of health, education, food, comms and
-# happiness. 
+# happiness.
 
 # The first class I define is Country, with configurable characteristics.
 class Country
-	def initialize(country)
-		@country = country
-		@health = health
-		@knowledge = knowledge
-		@hunger = hunger
-		@comminication = communication
-		@happiness = happiness
+	def initialize(options = {})
+		@name = options[:name]
+		@health = options[:health]
+		# @knowledge = knowledge
+		# @hunger = hunger
+		# @communication = communication
+		@happiness = options[:happiness]
 	end
+
+  def happy?
+    @happiness
+  end
+
+  def sad?
+    not happy?
+  end
 end
+
+britain = Country.new(:name => 'Britain', :happyness => true)
+puts britain.sad?
+exit
 
 # Here I am creating a class called Services, which, depending on the
 # values in a country's characteristics, will attempt to bring them
@@ -22,26 +34,31 @@ class Services
 	def provide_universal_health_care
 		if @health == "bad"
 			puts "We are sending doctors, nurses, and bandages #{@country}."
+    end
 	end
 
 	def provide_free_education
 			if @knowledge == "bad"
 			puts "Welcome to the classroom #{@country}. Don't forget to do your homework!!"
+    end
 	end
 
 	def provide_delicious_food
 		if @hunger == "bad"
 			puts"Ready for blutwurst #{@country}?"
+    end
 	end
 
 	def provide_hi_speed_internet
 		if @communication == "bad"
 			puts "The lolcats are coming #{country}:-)"
+    end
 	end
 
 	def put_ecstacy_in_the_water
 		if @happiness == "bad"
 			puts "Just dance #{@country}!"
+    end
 	end
 end
 
