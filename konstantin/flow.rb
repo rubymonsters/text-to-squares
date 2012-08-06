2132c0660e2dad214bbcf662e1f995856ead8594
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2012-08-06 21:20:47 +0200

    use none?

--- konstantin/flow.rb
+++ konstantin/flow.rb
@@ -18,7 +18,7 @@ britain   = Country.new("britain", true)
 germany   = Country.new("germany", false)
 countries = [spain, britain, germany]
 
-world_is_happy = countries.any? do |c|
+world_is_happy = countries.none? do |c|
   c.happy?
 end
 
