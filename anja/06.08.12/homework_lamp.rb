b175c0c70f2319c55e91512ff6cf32b609fd5b56
Tyranja <Tyranja@cassiopeia.uberspace.de>
2012-08-09 19:04:58 +0200

    instance variablen and array

--- anja/06.08.12/homework_lamp.rb
+++ anja/06.08.12/homework_lamp.rb
@@ -2,10 +2,13 @@
 class Person
 	def initialize(name)
 		@name = name
+		Rooms = ["kitchen", "floor", "bathroom", "dinning room"]
+
 	end
 
-	def moving
-		#moves into rooms and turns the light on
+	def move
+		person = Room.new
+		person.enter_room(Room.shuffle)		#moves into rooms and turns the light on
 	end
 end
 
@@ -14,32 +17,37 @@ class Room
 		@name = name
 		@lamp = Lamp.new
 	end
+
+	def enter_room
+		puts @name + "goes into the " + @name
+	end
+
 end
 
 class Lamp
 	def initialize()
 		@bulb = Bulb.new
-		@Switch = Switch.new
+		@switch = Switch.new
 	end
 end
 
 class Switch
 	def initialize(state)
-		@switch_state	'on or off (true or false)'	
+		@switch_state = state	#on or off (true or false)	
 	end
 
 	def flip
-
+		@switch_state = not @state
 	end
 end
 
 class Bulb
 	def initialize()
-		@bulb_state 'broken or not broken'	
+		@bulb_state #broken or not broken
 	end
 
 	def broken
-
+		puts 
 	end 
 end	
 	
