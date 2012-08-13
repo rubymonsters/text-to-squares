5ab9b97bbf0eb91ef219d1752535abd025f4d235
Sven Fuchs <me@svenfuchs.com>
2012-08-13 20:46:30 +0200

    example of a method that changes internal state

--- foo.rb
+++ foo.rb
@@ -6,12 +6,21 @@ class Switch
   def state
     @state
   end
+
+  def flip
+    if @state == :off
+      @state = :on
+    else
+      @state = :off
+    end
+
+    # could also use a ternary operator:
+    # @state = @state == :off ? :on : :off
+  end
 end
 
 switch = Switch.new(:off)
-if switch.state == :on
-  puts "the switch is on"
-else
-  puts "the switch is off"
-end
+# let's set the internal state of the
+# switch to :on
+switch.flip
 
