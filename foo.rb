92dc91423f4805830654952d2768640b0f418e42
Sven Fuchs <me@svenfuchs.com>
2012-08-13 22:00:04 +0200

    add an example of a class level method

--- foo.rb
+++ foo.rb
@@ -1,20 +1,54 @@
-class Switch
+class Room
+  class << self
+    def names
+      @names ||= ['Kitchen', 'Dining room', 'Office']
+
+      # this is the same as:
+      #
+      # if @names
+      #   @names
+      # else
+      #   @names = ['Kitchen', 'Dining room', 'Office']
+      # end
+    end
+  end
+
   def initialize
-    @state = false
+    @name = Room.names.shuffle!.pop
   end
-
-  def flip
-    # toggle the state
-  end
-
-  def turn_off
-    @state = false
-  end
-
-  def turn_on
-    @state = true
-  end
+  # that's the same as:
+  #
+  # def self.names
+  #   ['Kitchen', 'Dining room', 'Office']
+  # end
 end
 
-switch = Switch.new
-switch.turn_on
+p Room.names
+p Room.names.pop
+p Room.names
+p Room.names.pop
+p Room.names
+p Room.names.pop
+
+
+
+# class Switch
+#   def initialize
+#     @state = false
+#   end
+#
+#   def flip
+#     # toggle the state
+#   end
+#
+#   def turn_off
+#     @state = false
+#   end
+#
+#   def turn_on
+#     @state = true
+#   end
+# end
+#
+# switch = Switch.new
+# switch.turn_on
