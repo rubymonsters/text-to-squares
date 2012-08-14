c4cfe385bccafee1d41d3cca9eef9da7284564f7
Tyranja <Tyranja@cassiopeia.uberspace.de>
2012-08-14 23:22:51 +0200

    lamp_home work verändert, funktioniert noch nicht

--- anja/06.08.12/homework_lamp.rb
+++ anja/06.08.12/homework_lamp.rb
@@ -3,18 +3,17 @@ class Person
 	def initialize(name)
 		@name = name
 		@room = Room.names.shuffle!.pop
-		
+		@lamp = Lamp.new
+	end
+
+	def room
+		@room
 	end
 
 	def enter_room
-		puts @name + " goes into the " + @room + "."
-		turn_light_on
-	end
-
-	def turn_light_on
-		puts "Its dark. " + @name +" turns the light on."
-		@lamp = Lamp.new(@room)
-		@lamp.flip_switch
+			puts @name + " goes into the " + @room + "."
+			puts "Its dark. " + @name +" turns the light on."
+			@lamp.light_comes_on 
 	end
 end
 
@@ -25,64 +24,48 @@ class Room
 end
 
 class Lamp
- 	def initialize(room)
- 		@switch = true   #Switch.new(true)
- 		@bulb = true	#Bulb.new
- 		@room = room
-
+ 	def initialize()
+ 		@switch = Switch.new
  	end
 
- 	def flip_switch()
-		if @bulb == true   #statt @bulb  @bulb.on == true
- 			puts "Now the light in the #{@room} is on."	#?didnt know how to take the var @room from class Person
- 		else
-			puts "The light in the #{@room} was already on. You turned it off."
- 		end
+ 	def light_comes_on
+ 		if @switch.state == :on
+			puts "The light in the " +  @room + " comes_on"
+		elsif @switch.state == :off
+			puts "The Light was already on. You turned it off."
+		else
+			puts "Nothing works"
+		end
  	end
- end
-
+end
 
 class Switch
- 	def initialize(state)
- 		@state = state					#true or false	
- 	end
+  def initialize
+    @state = state   #internal(private) variable
+  end
 
- 	def state
- 		@state
- 	end
+  def state           #expose the variable to external world
+    @state            #attribute reader
+  end
 
- 	def flip
- 		if @state == true
- 			@state = false				#switch is off => bulp is off
- 		else
- 			@state = true				#switch is on => bulp is on
- 		end
- 		# @state = not @state, doesnt work
- 	end
- end
-
- class Bulb
- 	def initialize()
- 	end
-
- 	def on
- 		if Switch.state == true			#var @state aus der switch klasse 
- 			return true					#Bulb is on
- 		else
- 			return false				#Bulb is off
- 		end
- 	end
-
- end
-
-
-
-# 	def broken
-# 		puts "You broke the light in the #{@room}."
-# 	end 
-# end	
-	
+  def flip
+    if @state == :off
+      @state = :on
+    else
+      @state = :off  
+    end
+  end
+end
 
 
 Anja = Person.new("Anja")
 Anja.enter_room
+
+
+
+
+
+Anja = Person.new("Anja")
+Anja.enter_room
+Anja.room
+ 
