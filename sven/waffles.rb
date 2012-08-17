cf54198bfb5f84c2209937ab468d5f029f428483
Sven Fuchs <me@svenfuchs.com>
2012-08-17 15:46:27 +0200

    railsgirls exercises, 2

--- sven/waffles.rb
+++ sven/waffles.rb
@@ -38,14 +38,15 @@ class Iron
 
   def ready?
     @state == :hot
+    
   end
 
   def receive(dough)
-    if ready?
+    #if ready?
       @waffle = Waffle.new(dough)
-    else
-      puts "Don't add dough to a cold iron!"
-    end
+      #else
+      #puts "Don't add dough to a cold iron!"
+      #end
   end
 
   def bake
@@ -54,17 +55,49 @@ class Iron
       @waffle
     else
       puts "Can't bake with a cold iron!"
+     @waffle
     end
+  end
+end
+
+class Topping
+  def initialize(toppings)
+    @topping = toppings.shuffle.pop
+    @topping
+  end
+  
+  def choose
+    
+  end
+  
+  def place
+    "Put  #{@topping}  on the waffle!"
   end
 end
 
 ingredients = [:flour, :eggs, :sugar, :butter]
 dough = Dough.new(ingredients)
+if dough.has?(:milk)
+  puts "has milk"
+else
+  puts "has no milk"
+end
 
 iron = Iron.new
 iron.heatup
 iron.receive(dough)
+# iron.ready?
+#else
+#  puts "not ok"
+#end
 waffle = iron.bake
 
-p waffle.done?
+if waffle.done?
+  toppings = [:chocolate, :marmelade, :fruits]
+  topping = Topping.new(toppings)
+  p topping.place 
+else
+  puts "No waffle done"
+end
 p waffle.dough.ingredients
+
