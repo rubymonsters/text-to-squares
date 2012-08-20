791cdd5abe2e4d013c17d512366218dfbff73804
Reb <bexi@freenet.de>
2012-08-20 17:39:00 +0200

    updated

--- Rebecca/dogs.rb
+++ Rebecca/dogs.rb
@@ -1,69 +1,63 @@
-#creating dogs and let them ask a question
 
 class Dogs
-  def initialize (options ={})
-    @name = options [:name]
-    @happy = options [:happy]
-    @sad = options [:sad]
-    @speak = options [:speak]
+  def initialize(name)
+    @name = name
+    @petted = 0
+    # @sticks_thrown = 0
+    puts "#Dog #{@name} is created. Times petted yet: #{@petted} times." 
   end
 
-  def introduce
-  puts @speak
-  puts  "My name is " + @name.upcase + " and the name is " + @name.length.to_s + " characters long."
+  def name
+    @name
   end
 
-  def look(*look)   
-    puts "More important facts about me:"
-  look.each do |look|
-    puts "I am " + look + "!"
+  def introduction(gender, age, *look)
+    "Meet dog #{@name}. He is #{gender}, #{age} years old, #{look.join(', ')}."
   end
 
-  def happy
-  @happy
+   def pet
+    @petted = @petted + 1
+     puts "#{@name} is petted!"
   end
 
-  def sad
-  @sad
+  def name
+    @name
   end
 
-  def speak
-    @speak
+  def petted
+    @petted
   end
-
-  def behaviour
-    
-    puts "Will you pet me?"
-  	reply = gets.chomp.downcase
-
-  	if reply == "yes"
-   3.times do 
-   puts @speak + ". " + @name + " is " + @happy 
-   end
-   puts "[" + @name + " jumps on your lap and you are friends forever]"
-
-elsif reply == "no" 
-	puts "'Roaaarrrrr' [" + @name + " is " + @sad + " and attacks you! Better run!]"
-	else puts "Please type 'yes' or 'no'." 
-		behaviour
-		end
-   end
-end
 end
 
 
-doodle = Dogs.new(:name => 'Doodle',:happy => "happy", :sad => "sad",:speak => "'Wau Wau'")
-bingo = Dogs.new(:name => 'Bingo', :happy => "happy", :sad => "sad", :speak => "'Bow Wow'")
+dogs = [Dogs.new('Bingo'), Dogs.new('Doodle')]
+
+puts dogs[0].introduction("male", 7, "hairy", "clumsy", "furious", "old")
+puts dogs[1].introduction("female", 2, "nosy", "fast", "playful")
+
+5.times do
+  dogs.each { |dog| dog.pet if rand > 0.5 }
+end
+
+dogs = dogs.sort_by { |dog| dog.petted}
+#^^#dogs = dogs.sort { |dog| dog.petted }  <---this gave me a warning:
+#(warning: multiple values for a block parameter (2 for 1). so I put sort_by and it worked I guess?
+dog1 = dogs[0]
+dog2 = dogs[1]
+#dog = dogs.first 
+#^^I didn´t use it because I wanted both dogs to display the amount petted,
+#Have to find out how to do this for both. 
+puts "#Total amount of dogs petted: #{dog1.petted + dog2.petted} times."
+puts "##{dog1.name} was petted #{dog1.petted} times!"
+puts "##{dog2.name} was petted #{dog2.petted} times!"
 
 
-doodle.introduce
-doodle.look("from Germany", "hairy", "clumsy", "slow", "furious",)
-doodle.behaviour
-
-bingo.introduce
-bingo.look( "from England", "big", "fast", "smart", "loyal")
-bingo.behaviour
-
-#question: how can I have both (all that are created) dogs "speak" in turns?
-
+if dog1.petted == dog2.petted
+  puts "#Both dogs were petted equally."
+elsif
+  dog1.petted <= dog2.petted
+  puts "##{dog2.name} was petted most."
+else
+  puts "##{dog1.name} was petted most."
+end
 
