921e03f0a1eb7866b32d2211d77a43fff4146593
Tyranja <Tyranja@cassiopeia.uberspace.de>
2012-08-24 13:04:32 +0200

    3 tests are working

--- anja/plant_testing.rb
+++ anja/plant_testing.rb
@@ -1,40 +1,79 @@
 require 'test/unit'
 
 class Person
-
-  attr_reader :name
-
-  def initialize(name) #name der person
+  def initialize(name)
+    @basilikum = Plant.new
+    @watering_can = WateringCan.new
     @name = name
   end
 
-  def watering  #case plant = dry => pour water
+  attr_reader :name
+
+  def need_to_water?  #case plant = dry => pour water
+    if @basilikum.status == :dry 
+      WateringCan.pours_water
+    else
+      puts "The Plant has enough water!"
+    end
   end
 end
 
 
 class Plant
+  attr_reader :status
 
   def status    #check if dry or not
+    :dry
   end
 
   def grow        #if wet => grow
   end
 end
 
-class WateringCan
+class WateringCan #could also be self class
 
-  def pours_water  #pour water if plant dry
+  def self.pours_water  #pour water if plant dry
+    puts "The watering can pours water."
   end
-
 end
+
+
 
 class WateringTest < Test::Unit::TestCase
   def test_name
-    anja = Person.new("Anja")
-    assert anja.name, 'create a Person named Anja'
-    
+    gardener = Person.new("Gardender")
+    assert gardener.name, 'create a Person named gardener'
   end
+
+  def test_need_to_water?
+    gardener = Person.new("Gardener")
+    if gardener.basilikum.status == :dry
+      assert gardener.pours_water, 'takes the can and gives water to the plant,if plant is dry'
+    end
+  end
+
+  # def test_watering_if_plant_wet
+  #   gardener = Person.new("Gardener")
+  #   if @basilikum.status != :dry
+  #     assert puts "The Plant has enough water!", 'takes the can and gives water to the plant,if plant is dry'
+  #   end
+  # end
+
+  def test_wateringCan
+    watering_can = WateringCan.new
+    assert watering_can, 'watering can does pour water'
+  end
+
+  def test_plant_status
+    basilikum = Plant.new
+    assert basilikum.status
+  end
+
+  # def test_plant_grow_if_plant_wet
+  # end
+
+  # def test_plant_grow_if_plant_dry
+  # end
 end
 
 #Person knows about watering_can and plant
