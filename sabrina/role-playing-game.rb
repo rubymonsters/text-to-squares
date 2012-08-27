7eb7e7aa999f5947f5dd59771d93b5f772dee7fc
sabrina-ulbrich <su@evolos.de>
2012-08-27 14:35:51 +0200

    rivised role-playing-game and tests

--- sabrina/role-playing-game.rb
+++ sabrina/role-playing-game.rb
@@ -1,3 +1,4 @@
+# Prepare
 # Come up with an idea for a program that contains at least two classes (a role playing game character)
 # Create an empty file, require "test/unit" and add empty class definitions for your new classes.
 # Write down your expectations about what the classes do in plain text/comments at the end of the file:
@@ -11,34 +12,60 @@
 require "test/unit"
 
 class Character
-  def eat(food)
-    # if there is food the character can eat it, after a while (like 5 actions) he needs food
+  attr_reader :stamina, :conjure_skill, :fight_skill
+  
+  def initialize(stamina, conjure_skill, fight_skill)
+    # if a character is created its stamina is set to 10 points
+    # its conjure- and fight-skills are set to 1
+    @stamina = stamina
+    @conjure_skill = conjure_skill
+    @fight_skill = fight_skill
+  end
+  
+  def eat
+    # after eating the characters stamina is set to 10 points
+    @stamina = 10
   end
   
   def go
-    # the character can go, he can go faster if he has a map
+    # after going the characters stamina loses 1 point
+    # some idea for later: the character can go, he can go faster if he has a map
+    @stamina -= 1
   end
   
   def conjure
-    # the character can conjure if he has a spell-book
+    # after conjuring the characters stamina loses 2 points
+    # some idea for later: the character can conjure if he has a spell-book
+    @stamina -= 2
+    @conjure_skill += 1
   end
   
   def fight
+    # after fighting the characters stamina loses 3 points
     # the character can fight, in the simplest case with his fists
   end
   
-  def ask_questions
-    # the character can ask questions to other characters
+  def use(item)
+    # depending on what item is used, the specific activity improves
+    # if the character uses an item for fighting the chance to win increases
   end
 end
 
 class Equipment
-  def use(item)
-    # depending on what item is used, the specific activity improves
+  attr_reader :status
+  
+  def initialize(status)
+    @status = status
   end
   
-  def broken?
+  def status
     # if equipment is broken it can't be used
+    # some idea for later: equipment can be used for learning magic spells or better fighting
+    if @status == 0
+      p "You can't use the item because it's broken"
+    else
+      @status
+    end
   end
 end
 
@@ -48,15 +75,74 @@ class Food
   end
 end
 
-# A Character can eat, go, conjure, fight and ask_questions; he can use equipment to act better
-# Equipment can be used for learning magic spells or better fighting
-# Food can be eaten and refreshes a character if its eatable
+# Test
+# Turn each line of the comment-list into an empty test method.
+# Run the file, the test output should change.
+# Implement one of the tests by expressing your expectation (e.g. charachter = Character.new; assert_equal 1, character.level)
+# Run the file, the test output should change.
 
 class CharacterTest < Test::Unit::TestCase
+  def test_character_creation
+    # if a character is created its stamina is set to 10 points
+    # its conjure- and fight-skills are set to 1
+    character = Character.new(10, 1, 1)
+    assert character, 'create a character instance'
+  end
+  
+  def test_eat_stamina_is_set_to_10_points
+    character = Character.new(0, 1, 1)
+    character.eat
+    assert_equal character.stamina, 10, 'characters stamina should be set to 10 points'
+  end
+  
+  def test_go_stamina_loses_1_point
+    character = Character.new(10, 1, 1)
+    character.go
+    assert_equal character.stamina, 9, 'characters stamina should loose 1 point'
+  end
+  
+  def test_conjure_stamina_loses_2_points
+    character = Character.new(10, 1, 1)
+    character.conjure
+    assert_equal character.stamina, 8, 'characters stamina should loose 2 points'
+  end
+  
+  def test_conjure_skill_increases_by_1_point
+    character = Character.new(10, 1, 1)
+    character.conjure
+    assert_equal character.conjure_skill, 2, 'characters conjure-skill should increase by 1 point'
+  end
+  
+  def test_fight
+    # after fighting the characters stamina loses 3 points
+    # its fighting-skill increases by 1 point
+  end
+  
+  def test_use
+    # if the character uses an item the chance to win increases
+  end
 end
 
 class EquipmentTest < Test::Unit::TestCase
+  def test_equipment_creation
+    # if equipemt is created its status is set to 10
+    equipment = Equipment.new(10)
+    assert equipment, 'create an equipment instance'
+  end
+  
+  def test_status_is_0_it_can_not_be_used
+    equipment = Eqipment.new(0)
+    assert_equal equipment.status, 0, 'equipment can not be used because it is broken'
+  end
+
+  def test_status_is_not_0_it_can_be_used
+    equipment = Eqipment.new(10)
+    assert_not_equal equipment.status, 0, 'equipment can be used'
+  end
 end
 
 class FoodTest < Test::Unit::TestCase
+  def test_eatable?
+    # food can be eatable or not
+  end
 end
