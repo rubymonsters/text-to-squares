babb710e4f1c5cc0bb9ae9603ed1fff2eeab6056
Sven Fuchs <me@svenfuchs.com>
2012-09-02 23:52:29 +0200

    add tests

--- sven/text_to_squares.rb
+++ sven/text_to_squares.rb
@@ -30,7 +30,6 @@
 #
 # - knows all the available colors (this is common class knowledge, so it
 #   should be a class method)
-# - normalizes the string (strip off invalid characters and downcase)
 # - has a method `picture` which returns an instance of `Picture` created
 #   with that string
 # - has a method squares which returns the squares from the picture
@@ -40,7 +39,7 @@
 # - takes the string as well as a number of row and columns
 #   as arguments of the initalize method
 # - has a method `normalized_string` which returns a version of the string with
-#   everything but A-Za-z stripped off
+#   everything but A-Za-z stripped off and downcased
 # - has a method `characters` which returns all the characters from the
 #   normalized string
 # - has a method `squares` which returns an array of `Square` instances where
@@ -50,4 +49,72 @@
 #
 # - takes a letter as an argument of the initialize method
 # - has a method `color` which returns the color according to the letter
-# - has a fixed size
+
+class ApplicationTest < Test::Unit::TestCase
+  def test_colors_returns_a_hash
+    color = Application.colors
+    # assert that colors is a hash
+  end
+
+  def test_colors_returns_a_hash_containing_a_value_for_a
+    color = Application.colors['a']
+    # assert that color is the color which is assigned to "a"
+  end
+
+  def test_creation
+    app = Application.new('something')
+    # assert that application is an instance of Application
+  end
+
+  def test_picture_returns_a_picture
+    app = Application.new('something')
+    # assert that the method picture returns an instance of Picture
+  end
+
+  def test_squares_returns_an_array_of_squares
+    app = Application.new('something')
+    # assert that application.squares returns an array where the first object is an instance of Square
+    # hint: you will need to implement the Picture and Square classes first
+  end
+end
+
+class PictureTest < Test::Unit::TestCase
+  def test_creation
+    picture = Picture.new('something')
+    # assert that picture is an instance of Picture
+  end
+
+  def test_normalized_string_strips_special_chars
+    picture = Picture.new('something!')
+    # assert that picture.normalized_string returns a string with "!" stripped off
+  end
+
+  def test_normalized_string_downcases
+    picture = Picture.new('SOMETHING')
+    # assert that picture.normalized_string returns a string with "!" stripped off
+  end
+
+  def test_characters_returns_an_array_of_characters_from_the_normalized_string
+    picture = Picture.new('something')
+    # assert that picture.characters returns an array of all characters contained in "something"
+  end
+
+  def test_squares_returns_an_array_of_squares_from_the_characters
+    picture = Picture.new('something')
+    # assert that picture.squares returns an an array of Square instances where the first one
+    # has the letter "s"
+    # hint: you will need to implement the Square class first
+  end
+end
+
+class SquareTest < Test::Unit::TestCase
+  def test_creation
+    square = Square.new('a')
+    # assert that square is an instance of Square
+  end
+
+  def test_color_returns_the_color_assigned_to_the_character
+    square = Square.new('a')
+    # assert that square.color returns the color which is assigned to "a"
+  end
+end
