bc20230c7a5bd3ec63d2316013ec52e584825a81
Sven Fuchs <me@svenfuchs.com>
2012-09-10 21:13:06 +0200

    parts of next week's homework

--- app/run.rb
+++ app/run.rb
@@ -9,9 +9,12 @@ print 'please input a string: '
 string = gets
 
 app = Application.new(string)
-template = File.read('lib/template.html.erb')
-erb = ERB.new(template)
-html = erb.result(binding)
+
+# template = File.read('lib/template.html.erb')
+# erb = ERB.new(template)
+# html = erb.result(binding)
+
+html = app.render
 
 File.open('index.html', 'w') { |file| file.puts(html) }
 
