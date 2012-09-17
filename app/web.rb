8f9dd2947ba5058a90e2c905a4aee1654511b941
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2012-09-17 20:53:44 +0200

    add Sinatra application

---
+++ app/web.rb
@@ -0,0 +1,32 @@
+$: << 'lib'
+
+require 'sinatra'
+require 'erb'
+require 'application'
+require 'picture'
+require 'square'
+
+get '/' do
+  "
+  <form method='post'>
+    <textarea name='input'></textarea>
+    <input type='submit' />
+  </form>
+  "
+end
+
+post '/' do
+  string    = params[:input]
+  app       = Application.new(string)
+  template  = File.read('lib/template.html.erb')
+  erb       = ERB.new(template)
+  erb.result(binding)
+end
+
+get '/:input' do
+  string    = params[:input]
+  app       = Application.new(string)
+  template  = File.read('lib/template.html.erb')
+  erb       = ERB.new(template)
+  erb.result(binding)
+end
