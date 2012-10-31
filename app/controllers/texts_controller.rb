38e045db35ca5c8428a4e7eaf941fa793f96b4dc
Tyranja <tyranja@cassiopeia.uberspace.de>
2012-10-31 20:45:10 +0100

    text with user coneccet

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -41,6 +41,7 @@ class TextsController < ApplicationController
   # POST /texts.json
   def create
     @text = Text.new(params[:text])
+    @text.user = current_user
 
     respond_to do |format|
       if @text.save
