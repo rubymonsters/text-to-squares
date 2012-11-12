eba0025b798d1e981721c4dbfaab6f84d4eeb5dc
Matt Patterson <matt@reprocessed.org>
2012-11-12 19:13:09 +0100

    Add simplest possible test for User#is_admin?

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :twitter_id, :twitter_avatar
 
   has_many :texts
+
+  def is_admin?
+    false
+  end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  test "a new user is not an admin" do
+    user = User.new
+    assert user.is_admin? == false, "New user should not be an admin"
+  end
+
   # test "the truth" do
   #   assert true
   # end
