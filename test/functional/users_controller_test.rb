26b749679706929339e163d6e544f34ea66f0813
Tyranja <tyranja@cassiopeia.uberspace.de>
2012-11-22 23:33:14 +0100

    all tests are working

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -6,22 +6,21 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should get show" do
-    get :show
+    get :show, :id => @user.id
     assert_response :success
     #assert_not_nil assigns(:users)
-
   end
 
   test "should have class table" do
-    get :show   
+    get :show, :id => @user.id   
     assert_select '.table' #css selector
   end
 
   test "show view displays user name" do
     @user.name = "Maren"
     @user.save!
-    get :show
-    # puts response.body
+    get :show, :id => @user.id
+    puts response.body
     # puts Text.all.map{|text| [text.input, text.user.try(:screen_name), text.created_at]}
     # puts User.all.map{|user| user.name}
     assert_select "h1", "All the Texts from Maren."
