4b46edc4c6e993894b6c45e3173cd4da3f061213
Tyranja <tyranja@cassiopeia.uberspace.de>
2012-11-29 13:07:03 +0100

    helper method for current user equals text user

--- app/helpers/texts_helper.rb
+++ app/helpers/texts_helper.rb
@@ -1,2 +1,12 @@
 module TextsHelper
+
+  def helper_current_user_equals_text_user?(current_user,text)
+    if (current_user && text.user && current_user.id == text.user.id)
+      true
+    else
+      false
+    end
+  end
+
+
 end
