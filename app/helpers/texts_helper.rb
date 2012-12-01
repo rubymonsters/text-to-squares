6441410b5286350be48bf9bfb93aae5ffb327ecd
Tyranja <tyranja@cassiopeia.uberspace.de>
2012-12-01 09:34:35 +0100

    change helper method name

--- app/helpers/texts_helper.rb
+++ app/helpers/texts_helper.rb
@@ -1,6 +1,6 @@
 module TextsHelper
 
-  def helper_current_user_equals_text_user?(current_user,text)
+  def can_edit_text?(current_user,text)
     if (current_user && text.user && current_user.id == text.user.id)
       true
     else
