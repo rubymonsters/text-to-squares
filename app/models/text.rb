d769572e8713a12372e57088f2dcc90828495875
Sven Fuchs <me@svenfuchs.com>
2012-12-10 19:22:56 +0100

    playing with different kinds of refactorings for moving the colorschema knowledge around

--- app/models/text.rb
+++ app/models/text.rb
@@ -4,15 +4,15 @@ class Text < ActiveRecord::Base #framework aus verschiedenen klassen/module
   validate  :input_should_not_be_empty
 
   belongs_to :user
-  
 
-  def self.colors_blue
-    { 'a' => '112233','b' => '223344','c' => '334455','d' => '445566','e' => '556677',
-      'f' => '667788','g' => '778899','h' => '8899AA','i' => '99AABB','j' => 'AABBCC',
-      'k' => 'BBCCDD','l' => 'CCDDEE','m' => 'DDEEFF','n' => 'EEFF11','o' => 'FF1122',
-      'p' => '123456','q' => '234567','r' => '345678','s' => '456789','t' => '56789A',
-      'u' => '6789AB','v' => '789ABC','w' => '89ABCD','x' => '9ABCDE','y' => 'ABCDEF',
-      'z' => 'BCDEF1' }
+
+  def self.colors(name)
+    { :blue => { 'a' => '112233','b' => '223344','c' => '334455','d' => '445566','e' => '556677',
+                'f' => '667788','g' => '778899','h' => '8899AA','i' => '99AABB','j' => 'AABBCC',
+                'k' => 'BBCCDD','l' => 'CCDDEE','m' => 'DDEEFF','n' => 'EEFF11','o' => 'FF1122',
+                'p' => '123456','q' => '234567','r' => '345678','s' => '456789','t' => '56789A',
+                'u' => '6789AB','v' => '789ABC','w' => '89ABCD','x' => '9ABCDE','y' => 'ABCDEF',
+                'z' => 'BCDEF1' } }
   end
 
   def self.colors_red
@@ -37,17 +37,25 @@ class Text < ActiveRecord::Base #framework aus verschiedenen klassen/module
   def picture
     Picture.new(input)
   end
-  
+
   def input_should_not_be_empty
     if picture.normalized_string.blank?
       errors.add(:input, "You should write something!")
     end
   end
 
+  def color_for(char)
+    color_schema_hash[char]
+  end
+
+  def color_schema_hash
+    self.class.colors(color_scheme)
+  end
+
   def squares
     picture.squares
   end
-  
+
   def rows
     picture.rows
   end
